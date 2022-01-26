e98b6312f4ffc3bea4ffd2183e91150dd60847d9
Mauro Lima <mauro.lima@itssolucoes.com.br>
2022-01-26 18:40:10 -0300

    adaptacao download-arquivos a nova api

--- src/app/service/produtos-catalogo/produto.catalogo.service.ts
+++ src/app/service/produtos-catalogo/produto.catalogo.service.ts
@@ -11,31 +11,31 @@ export class ProdutoCatalogoService {
 
   constructor(private http: HttpClient) { }
 
-  public urlUpload: string = `${environment.apiUrl}v1/produtocatalogo/imagem`;
+  public urlUpload: string = `${environment.apiUrl}produtocatalogo/imagem`;
   public imgUrl: string = `${environment.imgUrl}`;
 
   buscarTodosProdutos(): Observable<ProdutoCatalogo[]> {
-    return this.http.get<ProdutoCatalogo[]>(`${environment.apiUrl}v1/produtocatalogo`);
+    return this.http.get<ProdutoCatalogo[]>(`${environment.apiUrl}produtocatalogo`);
   }
 
   buscarProdutoDetalhe(id:string):Observable<ProdutoCatalogo>{
-    return this.http.get<ProdutoCatalogo>(`${environment.apiUrl}v1/produtocatalogo/${id}/detalhes`);
+    return this.http.get<ProdutoCatalogo>(`${environment.apiUrl}produtocatalogo/${id}/detalhes`);
   }
 
   excluirProduto(id:string):Observable<boolean>{
-    return this.http.delete<boolean>(`${environment.apiUrl}v1/produtocatalogo/${id}`);
+    return this.http.delete<boolean>(`${environment.apiUrl}produtocatalogo/${id}`);
   }
 
   criarProduto(produto:any):Observable<any>{
-    return this.http.post<any>(`${environment.apiUrl}v1/produtocatalogo`, produto);
+    return this.http.post<any>(`${environment.apiUrl}produtocatalogo`, produto);
   }
 
   atualizarProduto(produto:any):Observable<any>{
-    return this.http.put<any>(`${environment.apiUrl}v1/produtocatalogo`, produto);
+    return this.http.put<any>(`${environment.apiUrl}produtocatalogo`, produto);
   }
 
   excluirImagem(idProduto:string, idImagem:string):Observable<boolean>{
-    return this.http.delete<boolean>(`${environment.apiUrl}v1/produtocatalogo/imagem?idProduto=${idProduto}&idImagem=${idImagem}`);
+    return this.http.delete<boolean>(`${environment.apiUrl}produtocatalogo/imagem?idProduto=${idProduto}&idImagem=${idImagem}`);
   }
   
 }
