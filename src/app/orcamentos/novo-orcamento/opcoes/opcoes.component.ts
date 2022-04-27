fb43e5bab4b12866f481eeff78abec72ef4c12dd
Gabriel Prada Teodoro <Gabriel.Teodoro@itssolucoes.com.br>
2022-04-27 16:20:07 -0300

    Criação de validação de formas de pagamento para opção, remoção de opção de orçamento

--- src/app/orcamentos/novo-orcamento/opcoes/opcoes.component.ts
+++ src/app/orcamentos/novo-orcamento/opcoes/opcoes.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { StringUtils } from 'src/app/utilities/formatarString/string-utils';
+import { MensagemService } from 'src/app/utilities/mensagem/mensagem.service';
 import { TelaDesktopBaseComponent } from 'src/app/utilities/tela-desktop/tela-desktop-base.component';
 import { TelaDesktopService } from 'src/app/utilities/tela-desktop/tela-desktop.service';
 import { ItensComponent } from '../itens/itens.component';
@@ -14,12 +15,18 @@ export class OpcoesComponent extends TelaDesktopBaseComponent implements OnInit 
 
   constructor(public readonly novoOrcamentoService: NovoOrcamentoService,
     public readonly itensComponent: ItensComponent,
-    telaDesktopService: TelaDesktopService) {
+    telaDesktopService: TelaDesktopService,
+    public readonly mensagemService:MensagemService) {
     super(telaDesktopService);
   }
 
   stringUtils = StringUtils;
   ngOnInit(): void {
 
+  }
+
+  removerOpcao(index: number) {
+    this.novoOrcamentoService.orcamentoCotacaoDto.listaOrcamentoCotacaoDto.splice(index, 1);
+    this.mensagemService.showSuccessViaToast("Opção de orçamento removida com sucesso!");
   }
 }
