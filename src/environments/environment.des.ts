0ba4a30cce725cc4b42a037af0e81b203561e0b1
Mauro Lima <mauro.lima@itssolucoes.com.br>
2022-04-27 17:15:56 -0300

    add environment DES

--- src/environments/environment.des.ts
+++ src/environments/environment.des.ts
@@ -1,10 +1,11 @@
 export const environment = {
     production: false,
+    environmentName: 'des',
     autenticaStorageSession: false, //se false, gaurda no localstorage (o usuário pode escolher na tela)
     minutosRenovarTokenAntesExpirar: 60 * 24 * 4, //token emitido com 7 dias, renovamos 4 dais antes de vencer
     esperaAvisos: 3000,
     esperaErros: 5000,
-    apiUrl: 'http://arclubeorcamentocotacaoapi.itssolucoes.com.br/',
+    apiUrl: 'http://its-appdev02:3100/',
     imgUrl: 'http://10.255.0.90/img/',
     versaoApi: 'DEBUG'
 };
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,15 +1,10 @@
-// The file contents for the current environment will overwrite these during build.
-// The build system defaults to the dev environment which uses `environment.ts`, but if you do
-// `ng build --env=prod` then `environment.prod.ts` will be used instead.
-// The list of which env maps to which file can be found in `angular-cli.json`.
-
 export const environment = {
-  production: true,
-  autenticaStorageSession: false, //se false, gaurda no localstorage (o usuário pode escolher na tela)
-  minutosRenovarTokenAntesExpirar: 60 * 24 * 4, //token emitido com 7 dias, renovamos 4 dais antes de vencer
-  esperaAvisos: 3000,
-  esperaErros: 5000,
-  apiUrl: 'http://arclubeorcamentocotacaoapi.itssolucoes.com.br/',
-  imgUrl: 'http://10.255.0.90/img/',
-  versaoApi: 'DEBUG',
+    production: true,
+    autenticaStorageSession: false, //se false, gaurda no localstorage (o usuário pode escolher na tela)
+    minutosRenovarTokenAntesExpirar: 60 * 24 * 4, //token emitido com 7 dias, renovamos 4 dais antes de vencer
+    esperaAvisos: 3000,
+    esperaErros: 5000,
+    apiUrl: 'http://arclubeorcamentocotacaoapi.itssolucoes.com.br/',
+    imgUrl: 'http://10.255.0.90/img/',
+    versaoApi: 'DEBUG',
 };
