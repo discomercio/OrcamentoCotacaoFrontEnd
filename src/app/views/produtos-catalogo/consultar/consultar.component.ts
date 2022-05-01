f23895fe5cb99ddecca8be902ff84b49b4593d57
Gabriel Prada Teodoro <Gabriel.Teodoro@itssolucoes.com.br>
2022-05-01 16:48:22 -0300

    Filtro de listagem

--- src/app/views/produtos-catalogo/consultar/consultar.component.ts
+++ src/app/views/produtos-catalogo/consultar/consultar.component.ts
@@ -17,7 +17,7 @@ import { ProdutoTela } from '../../orcamentos/novo-orcamento/select-prod-dialog/
   templateUrl: './consultar.component.html',
   styleUrls: ['./consultar.component.scss']
 })
-export class ProdutosCatalogoConsultarComponent implements OnInit, AfterViewInit {
+export class ProdutosCatalogoConsultarComponent implements OnInit {
 
   constructor(private readonly produtoService: ProdutoCatalogoService,
     private readonly alertaService: AlertaService) { }
@@ -35,10 +35,6 @@ export class ProdutosCatalogoConsultarComponent implements OnInit, AfterViewInit
     this.criarTabela();
     this.buscarPropriedadesProdutosAtivos();
     this.buscarPropriedadesEOpcoesProdutosAtivos();
-  }
-
-  async ngAfterViewInit() {
-
   }
 
   criarTabela() {
@@ -62,32 +58,35 @@ export class ProdutosCatalogoConsultarComponent implements OnInit, AfterViewInit
     }).catch((r) => this.alertaService.mostrarErroInternet(r));
   }
 
-
-
   buscarPropriedadesEOpcoesProdutosAtivos() {
     this.produtoService.buscarPropriedadesEOpcoesProdutosAtivos().toPromise().then((r) => {
       if (r != null) {
-        this.montarFilrosDropDowns(r)
+        this.montarFiltrosDropDowns(r)
       }
     }).catch((r) => this.alertaService.mostrarErroInternet(r));
   }
+
+  armazenaFiltro: any[] = [];
   drops: any[] = new Array<any>();
-  montarFilrosDropDowns(proprieadades: any[]) {
-    
-    let p  = new Array<any>();
+  montarFiltrosDropDowns(proprieadades: any[]) {
+    let p = new Array<any>();
     p = proprieadades;
 
     let map = proprieadades.map(x => x.IdProdpriedade);
     let filtrado: any[] = map.filter(this.distinct);
 
     filtrado.forEach(x => {
-      
+
       let filtro = proprieadades.filter(y => y.IdProdpriedade == x);
-      let prop = {item: filtro[0].NomePropriedade , values:[]};
-      if(prop.item != "Incluir na calculadora VRF"){
-        prop.values[0] = "Selecione";
-        filtro.forEach(f=> {
-          prop.values.push(f.ValorPropriedadeOpcao)
+      let prop = { item: filtro[0].NomePropriedade, values: [] };
+
+      if (prop.item != "Incluir na calculadora VRF") {
+        let camposFiltro = { campo: prop.item, valor: "" };
+        this.armazenaFiltro.push(camposFiltro);
+        // prop.values[0].valor = "Selecione";
+        filtro.forEach(f => {
+          let v = { valor: f.ValorPropriedadeOpcao };
+          prop.values.push(v)
         });
         this.drops.push(prop)
       }
@@ -97,46 +96,131 @@ export class ProdutosCatalogoConsultarComponent implements OnInit, AfterViewInit
   private distinct = (value, index, self) => {
     return self.indexOf(value) === index;
   }
-  produtosTela = new Array<ProdutoTabela>();
+
+  produtosTabela = new Array<ProdutoTabela>();
+  produtosTabelaApoio = new Array<ProdutoTabela>();
   criarColunas() {
-    
+
     let filtrado = this.produtosPropriedadesAtivos.map(x => x.produto);
     let r: string[] = filtrado.filter(this.distinct);
 
     r.forEach(x => {
       let pp = this.produtosPropriedadesAtivos.filter(y => y.produto == x);
-      let existe = this.produtosTela.filter(f => f.produto == x);
+      let existe = this.produtosTabela.filter(f => f.produto == x);
       if (existe.length == 0) {
         if (pp && pp.length > 0) {
           let produtoTela = new ProdutoTabela();
           produtoTela.produto = pp[0].produto;
-          produtoTela.fabricante = pp[0].fabricante + " " + pp[0].fabricanteNome;
+          produtoTela.fabricante = pp[0].fabricante + " - " + pp[0].fabricanteNome;
           produtoTela.descricao = pp[0].descricao;
+          produtoTela.id = pp[0].id;
+          produtoTela.linhaBusca = produtoTela.produto + "/" + pp[0].fabricante + "/" + pp[0].fabricanteNome + "/" + produtoTela.descricao + "/";
 
           pp.forEach(e => {
-            let item = this.produtosPropriedadesAtivos.filter(x => x.produto == e.produto && x.nomePropriedade == "Linha de Produto")[0];
-            if (item) produtoTela.linhaProduto = item.valorPropriedade;
+            let item: ProdutoCatalogoItemProdutosAtivosDados = new ProdutoCatalogoItemProdutosAtivosDados();
+            if (!produtoTela.linhaProduto) {
+              item = this.produtosPropriedadesAtivos.filter(x => x.produto == e.produto && x.nomePropriedade == "Linha de Produto")[0];
+              if (item) {
+                produtoTela.linhaProduto = item.valorPropriedade;
+                produtoTela.linhaBusca += produtoTela.linhaProduto + "/";
+              }
+            }
 
-            item = this.produtosPropriedadesAtivos.filter(x => x.produto == e.produto && x.nomePropriedade == "Tipo da Unidade")[0];
-            if (item) produtoTela.tipoUnidade = item.valorPropriedade;
+            if (!produtoTela.tipoUnidade) {
+              item = this.produtosPropriedadesAtivos.filter(x => x.produto == e.produto && x.nomePropriedade == "Tipo da Unidade")[0];
+              if (item) {
+                produtoTela.tipoUnidade = item.valorPropriedade;
+                produtoTela.linhaBusca += produtoTela.tipoUnidade + "/";
+              }
+            }
 
-            item = this.produtosPropriedadesAtivos.filter(x => x.produto == e.produto && x.nomePropriedade == "Voltagem")[0];
-            if (item) produtoTela.voltagem = item.valorPropriedade;
+            if (!produtoTela.voltagem) {
+              item = this.produtosPropriedadesAtivos.filter(x => x.produto == e.produto && x.nomePropriedade == "Voltagem")[0];
+              if (item) {
+                produtoTela.voltagem = item.valorPropriedade;
+                produtoTela.linhaBusca += produtoTela.voltagem + "/";
+              }
+            }
 
-            item = this.produtosPropriedadesAtivos.filter(x => x.produto == e.produto && x.nomePropriedade == "Capacidade (BTU/h)")[0];
-            if (item) produtoTela.capacidade = item.valorPropriedade;
-
-
+            if (!produtoTela.capacidade) {
+              item = this.produtosPropriedadesAtivos.filter(x => x.produto == e.produto && x.nomePropriedade == "Capacidade (BTU/h)")[0];
+              if (item) {
+                produtoTela.capacidade = item.valorPropriedade;
+                produtoTela.linhaBusca += produtoTela.capacidade + "/";
+              }
+            }
+            produtoTela.linhaBusca += e.valorPropriedade + "/";
           });
-          this.produtosTela.push(produtoTela);
+          this.produtosTabela.push(produtoTela);
         }
       }
-
     });
+    this.produtosTabelaApoio = this.produtosTabela;
+  }
+
+  filtrar(e: HTMLInputElement, tipo: string) {
+
+    this.setarFiltros(tipo, e.value);
+    this.filtrarProdutos();
+  }
+
+  setarFiltros(tipo: string, valor: string) {
+    this.armazenaFiltro.forEach(x => {
+      if (x.campo == tipo) {
+        x.valor = !valor ? "" : valor;
+      }
+    });
+  }
+
+  filtrarProdutos() {
+    let temFiltro: boolean = false;
+    this.produtosTabela = new Array<ProdutoTabela>();
+    this.produtosTabela = this.produtosTabelaApoio;
+    let filter = this.armazenaFiltro.filter(x => x.valor != "");
+
+    filter.forEach(x => {
+      if (x.valor) {
+        temFiltro = true;
+        let produtos: ProdutoTabela[] = new Array<ProdutoTabela>();
+        
+        produtos = this.produtosTabela.filter(f => f.linhaBusca.includes(x.valor));
+        if (produtos.length > 0) {
+          this.produtosTabela = new Array<ProdutoTabela>();
+          produtos.forEach(p => this.produtosTabela.push(p));
+        }
+        else {
+          this.produtosTabela = new Array<ProdutoTabela>();
+        }
+        // if(this.produtosTabela.length <= 0){
+        //   produtos = this.produtosTabelaApoio.filter(f => f.linhaBusca.includes(x.valor));
+        //   if (produtos.length > 0) {
+        //     produtos.forEach(p => this.produtosTabela.push(p));
+        //   }
+        // }
+        // else {
+        //   produtos = this.produtosTabela.filter(f => f.linhaBusca.includes(x.valor));
+        //   if (produtos.length > 0) {
+        //     this.produtosTabela = new Array<ProdutoTabela>();
+        //     produtos.forEach(p => this.produtosTabela.push(p));
+        //   }
+        //   else{
+        //     this.produtosTabela = new Array<ProdutoTabela>();
+        //   }
+        // }
+
+
+      }
+    });
+
+    if (!temFiltro && this.produtosTabela.length == 0) {
+      this.produtosTabela = this.produtosTabelaApoio;
+    }
   }
 }
 
 export class ProdutoTabela {
+  id: number;
+  linhaBusca: string;
   produto: string;
   fabricante: string;
   descricao: string;
@@ -144,4 +228,5 @@ export class ProdutoTabela {
   tipoUnidade: string;
   voltagem: string;
   capacidade: string;
+
 }
