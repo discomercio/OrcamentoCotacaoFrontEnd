7ecd372a97b0e38ff1250622997f3b59b0f1d00b
Mauro Lima <mauro.lima@itssolucoes.com.br>
2022-05-02 09:38:40 -0300

    alterando backend environment DES

--- src/environments/environment.des.ts
+++ src/environments/environment.des.ts
@@ -5,7 +5,7 @@ export const environment = {
     minutosRenovarTokenAntesExpirar: 60 * 24 * 4, //token emitido com 7 dias, renovamos 4 dais antes de vencer
     esperaAvisos: 3000,
     esperaErros: 5000,
-    apiUrl: 'http://its-appdev02:3000/',
+    apiUrl: 'http://its-appdev02:3100/',
     imgUrl: 'http://its-appdev02/img/',
     versaoApi: 'DEBUG'
 };
