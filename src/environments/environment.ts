f6ef1a2537a0bc40bab70589c575006716325384
Mauro Lima <mauro.lima@itssolucoes.com.br>
2022-05-06 11:05:48 -0300

    alteracao outputpath ngbuild

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,8 +4,8 @@ export const environment = {
     minutosRenovarTokenAntesExpirar: 60 * 24 * 4, //token emitido com 7 dias, renovamos 4 dais antes de vencer
     esperaAvisos: 3000,
     esperaErros: 5000,
-    apiUrl: 'http://arclubeorcamentocotacaoapi.itssolucoes.com.br/',
-    // apiUrl: 'http://localhost:60877/',
+    // apiUrl: 'http://arclubeorcamentocotacaoapi.itssolucoes.com.br/',
+    apiUrl: 'http://localhost:60877/',
     imgUrl: 'http://its-appdev02/img/',
     versaoApi: 'DEBUG'
 };
