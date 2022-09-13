f18d61809958da251fb0943eca64c9115abd15da
Gabriel Prada Teodoro <Gabriel.Teodoro@itssolucoes.com.br>
2022-09-13 19:27:26 -0300

    Busca de cep por endereço

--- src/app/views/cep/cep-dialog/cep-dialog.component.ts
+++ src/app/views/cep/cep-dialog/cep-dialog.component.ts
@@ -1,35 +1,174 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
+import { SelectItem } from 'primeng/api';
+import { DynamicDialogConfig, DynamicDialogRef } from 'primeng/dynamicdialog';
 import { AlertaService } from 'src/app/components/alert-dialog/alerta.service';
+import { CepDto } from 'src/app/dto/ceps/CepDto';
 import { CepsService } from 'src/app/service/ceps/ceps.service';
+import { FormatarEndereco } from 'src/app/utilities/formatarEndereco';
+import { TelaDesktopBaseComponent } from 'src/app/utilities/tela-desktop/tela-desktop-base.component';
+import { TelaDesktopService } from 'src/app/utilities/tela-desktop/tela-desktop.service';
 
 @Component({
   selector: 'app-cep-dialog',
   templateUrl: './cep-dialog.component.html',
   styleUrls: ['./cep-dialog.component.scss']
 })
-export class CepDialogComponent implements OnInit {
+export class CepDialogComponent extends TelaDesktopBaseComponent implements OnInit {
 
   constructor(public readonly cepService: CepsService,
-    private readonly alertaService: AlertaService) 
-    { }
+    private readonly alertaService: AlertaService,
+    telaDesktopService: TelaDesktopService,
+    @Inject(DynamicDialogConfig) public option: DynamicDialogConfig,
+    public ref: DynamicDialogRef,) {
+    super(telaDesktopService);
+  }
 
+  formatarEndereco: FormatarEndereco = new FormatarEndereco();
+  lstUfs: SelectItem[] = [];
+  uf: string;
+  lstCidades: SelectItem[] = [];
+  lstFiltradaCidades: SelectItem[] = [];
+  cidade: SelectItem;
+  lstEnderecos: CepDto[] = [];
+  endereco: any;
+  carregando: boolean;
+  origem:string;
 
-  lstUf: string[] = [];
   ngOnInit(): void {
+    this.origem = this.option.data.origem;
+    this.carregando = true;
     this.buscarUfs();
   }
 
   buscarUfs() {
-    return this.cepService.BuscarUfs("publico").subscribe({
+    return this.cepService.BuscarUfs(this.origem).subscribe({
       next: (r: string[]) => {
         if (!!r) {
-          this.lstUf = r;
+          this.lstUfs = this.montarLista(r);
+          this.carregando = false;
         }
         else {
+          this.carregando = false;
           this.alertaService.mostrarMensagem("Erro ao carregar a lista de Estados!")
         }
       },
-      error: (r: string) => this.alertaService.mostrarErroInternet(r)
+      error: (r: string) => {
+        this.carregando = false;
+        this.alertaService.mostrarErroInternet(r)
+      }
     });
+  }
+
+  montarLista(lst: any[]) {
+    let retorno: SelectItem[] = [];
+    retorno.unshift({ value: "Selecione" });
+
+    lst.forEach(x => {
+      retorno.push({ value: x })
+    });
+
+    return retorno;
+  }
+
+  filtrarCidades(event) {
+    //in a real application, make a request to a remote url with the query and return filtered results, for demo we filter at client side
+    let filtered: any[] = [];
+    let query = event.query;
+
+    for (let i = 0; i < this.lstCidades.length; i++) {
+      let country = this.lstCidades[i];
+      if (country.value.toLowerCase().indexOf(query.toLowerCase()) == 0) {
+        filtered.push(country);
+      }
+    }
+
+    this.lstFiltradaCidades = filtered;
+  }
+
+  buscarCidades(): void {
+
+    this.lstEnderecos = [];
+    this.lstCidades = [];
+    if (!this.uf) {
+      this.alertaService.mostrarMensagem("Favor selecionar um Estado!");
+    }
+    else {
+      this.carregando = true;
+      this.cepService.BuscarLocalidades(this.uf, this.origem).subscribe({
+        next: (r: string[]) => {
+          if (!!r) {
+            this.carregando = false;
+            this.lstCidades = this.montarLista(r);
+            this.cidade = { value: "" };
+            this.endereco = "";
+          }
+          else {
+            this.alertaService.mostrarMensagem("Erro ao carregar a lista de Cidades!")
+          }
+        },
+        error: (r: string) => {
+          this.carregando = false;
+          this.alertaService.mostrarErroInternet(r);
+        }
+      });
+    }
+  }
+  buscarCepPorEndereco() {
+
+    this.lstEnderecos = [];
+
+    if (!this.endereco && !this.cidade.value) {
+      this.alertaService.mostrarMensagem("Favor digitar o endereco ou a localidade!");
+    }
+    if (!this.uf) {
+      this.alertaService.mostrarMensagem("Favor selecionar um Estado!");
+    }
+    else if (this.uf && !this.cidade.value) {
+      this.alertaService.mostrarMensagem("Favor digitar uma localidade!");
+    }
+    else {
+
+      this.carregando = true;
+
+      let achou = this.lstCidades.filter(x => x.value == this.cidade.value);
+
+      if (achou.length > 0) {
+        this.cepService.buscarCepPorEndereco(this.endereco, this.cidade.value, this.uf, this.origem).toPromise().then((r) => {
+          if (!!r) {
+            if (r.length > 0) {
+              this.lstEnderecos = r;
+              this.endereco = "";
+            }
+            else {
+              this.alertaService.mostrarMensagem("Erro ao carregar a lista de Endereços!")
+            }
+          }
+          this.carregando = false;
+        }).catch((e) => {
+          this.carregando = false;
+          this.alertaService.mostrarErroInternet(e);
+        })
+      }
+    }
+  }
+
+  filtrarPorUf(uf: string) {
+    if (!uf) {
+      this.alertaService.mostrarMensagem("Necessário selecionar um estado!")
+    }
+    else {
+      let lst = this.lstEnderecos.filter((estado) => {
+        return estado.Uf === uf;
+      });
+    }
+  }
+  addEndereco() {
+      if (this.endereco != null) {
+        this.ref.close(this.endereco);
+      }
+      return;
+  }
+
+  marcarLinha(e: Event) {
   }
 }
--- src/app/views/cep/cep/cep.component.ts
+++ src/app/views/cep/cep/cep.component.ts
@@ -1,9 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import { DialogService } from 'primeng/dynamicdialog';
 import { AlertaService } from 'src/app/components/alert-dialog/alerta.service';
+import { CepDto } from 'src/app/dto/ceps/CepDto';
 import { CepsService } from 'src/app/service/ceps/ceps.service';
 import { StringUtils } from 'src/app/utilities/formatarString/string-utils';
 import { TelaDesktopBaseComponent } from 'src/app/utilities/tela-desktop/tela-desktop-base.component';
 import { TelaDesktopService } from 'src/app/utilities/tela-desktop/tela-desktop.service';
+import { CepDialogComponent } from '../cep-dialog/cep-dialog.component';
 
 @Component({
   selector: 'app-cep',
@@ -14,7 +17,8 @@ export class CepComponent extends TelaDesktopBaseComponent implements OnInit {
 
   constructor(telaDesktopService: TelaDesktopService,
     public readonly cepService: CepsService,
-    private readonly alertaService: AlertaService) {
+    private readonly alertaService: AlertaService,
+    private readonly dialogService: DialogService) {
     super(telaDesktopService);
   }
 
@@ -52,28 +56,28 @@ export class CepComponent extends TelaDesktopBaseComponent implements OnInit {
       this.cep_retorno = "";
       return false;
     }
-    
+
     if (this.cep_retorno != undefined) {
       if (StringUtils.retorna_so_digitos(this.cep_retorno) == StringUtils.retorna_so_digitos(this.Cep)) {
         return;
       }
     }
-    if(this.Cep != undefined && this.Cep != ""){
+    if (this.Cep != undefined && this.Cep != "") {
       // this.zerarCamposEndEntrega();
       //vamos fazer a busca
       this.carregando = true;
-  
+
       this.cepService.buscarCep(this.Cep, null, null, null, "publico").toPromise()
         .then((r) => {
           this.carregando = false;
-  
+
           if (!r || r.length !== 1) {
             this.cep_retorno = "";
             // this.mostrarCepNaoEncontrado();
             return;
           }
           const end = r[0];
-  
+
           this.cep_retorno = this.Cep;
           if (!!end.Bairro) {
             this.Bairro = end.Bairro;
@@ -88,7 +92,7 @@ export class CepComponent extends TelaDesktopBaseComponent implements OnInit {
               this.Cidade = end.Cidade;
               this.temCidade = true;
             }
-  
+
           }
           if (!!end.Endereco) {
             this.Endereco = end.Endereco;
@@ -97,11 +101,11 @@ export class CepComponent extends TelaDesktopBaseComponent implements OnInit {
             this.Uf = end.Uf;
             this.temUf = true;
           }
-  
+
         }).catch((r) => {
           //deu erro na busca
           //ou não achou nada...
-          
+
           this.carregando = false;
           this.alertaService.mostrarErroInternet(r);
         });
@@ -126,49 +130,53 @@ export class CepComponent extends TelaDesktopBaseComponent implements OnInit {
 
     let options: any = {
       autoFocus: false,
-      width: "60em"
+      width: "60em",
       //não colocamos aqui para que ele ajuste melhor: height:"85vh",
+      data: { origem: "publico" }
     };
     if (!this.telaDesktop) {
       //opções para celular
       options = {
         autoFocus: false,
         width: "100vw", //para celular, precisamos da largura toda
-        maxWidth: "100vw"
+        maxWidth: "100vw",
         //não colocamos aqui para que ele ajuste melhor: height:"85vh",
+        data: { origem: "publico" }
       };
     }
-    // const dialogRef = this.dialog.open(CepDialogComponent, options);
+    const ref = this.dialogService.open(CepDialogComponent,
+      options);
 
-    // dialogRef.afterClosed().subscribe((result) => {
-    //   if (result) {
-    //     let end: CepDto = dialogRef.componentInstance.lstEnderecos[dialogRef.componentInstance.endereco_selecionado];
-        
-    //     if (!!end.Uf) {
-    //       this.Uf = end.Uf;
-    //       this.temUf = true;
-    //     }
-    //     if (!!end.Cidade) {
-    //       if (!!end.ListaCidadeIBGE && end.ListaCidadeIBGE.length > 0) {
-    //         this.temCidade = false;
-    //         this.lstCidadeIBGE = end.ListaCidadeIBGE;
-    //       }
-    //       else {
-    //         this.Cidade = end.Cidade;
-    //         this.temCidade = true;
-    //       }
+    ref.onClose.subscribe((resultado: CepDto) => {
+      debugger;
+      if (resultado) {
+        let end: CepDto = resultado;
 
-    //     }
-    //     if (!!end.Bairro) {
-    //       this.Bairro = end.Bairro;
-    //     }
-    //     if (!!end.Endereco) {
-    //       this.Endereco = end.Endereco;
-    //     }
+        if (!!end.Uf) {
+          this.Uf = end.Uf;
+          this.temUf = true;
+        }
+        if (!!end.Cidade) {
+          if (!!end.ListaCidadeIBGE && end.ListaCidadeIBGE.length > 0) {
+            this.temCidade = false;
+            this.lstCidadeIBGE = end.ListaCidadeIBGE;
+          }
+          else {
+            this.Cidade = end.Cidade;
+            this.temCidade = true;
+          }
 
-    //     this.Cep = end.Cep;
-    //     this.cep_retorno = end.Cep;
-    //   }
-    // });
+        }
+        if (!!end.Bairro) {
+          this.Bairro = end.Bairro;
+        }
+        if (!!end.Endereco) {
+          this.Endereco = end.Endereco;
+        }
+
+        this.Cep = end.Cep;
+        this.cep_retorno = end.Cep;
+      }
+    });
   }
 }
