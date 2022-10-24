5400ae600f1d39e75226158c0fc6457241aad158
Kleber <kleber.lunguinho@itssolucoes.com.br>
2022-10-24 13:14:25 -0300

    Incluído dto de Operacao

---
+++ src/app/dto/operacao/operacao.ts
@@ -0,0 +1,11 @@
+export class Operacao {
+    id : number;
+    modulo: string;
+    tipo_operacao: string;
+    ordenacao: string;
+    descricao: string;
+    timestamp: string;
+    st_inativo: boolean;
+    st_oculto: boolean;
+}
+
