fed4251c4484f3281b4a4eb39ce23a764d1ca102
Kleber <kleber.lunguinho@itssolucoes.com.br>
2023-05-05 08:15:47 -0300

    Versão 1.0.0.19

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production : true,
-    version: "1.0.0.18"
+    version: "1.0.0.19"
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production : false,
-    version: "1.0.0.18"
+    version: "1.0.0.19"
 }
