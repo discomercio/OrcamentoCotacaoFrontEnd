986160da085c7d6d1ece4c57e0f50948b782f8dc
Kleber <kleber.lunguinho@itssolucoes.com.br>
2023-06-06 10:14:21 -0300

    Versão 1.0.0.27

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production : true,
-    version: "1.0.0.26"
+    version: "1.0.0.27"
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production : false,
-    version: "1.0.0.26"
+    version: "1.0.0.27"
 }
