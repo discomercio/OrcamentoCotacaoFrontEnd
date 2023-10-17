5087f695991caa3079128d25ff84c2190e353c8c
Gabriel Prada Teodoro <Gabriel.Teodoro@itssolucoes.com.br>
2023-10-16 21:01:04 -0300

    Alteração na forma de selecionar as evaporadoras para calculadora VRF

--- src/app/views/calculadora-vrf/select-evap-dialog/select-evap-dialog.component.ts
+++ src/app/views/calculadora-vrf/select-evap-dialog/select-evap-dialog.component.ts
@@ -1,13 +1,15 @@
-import { Component, Inject, OnInit, ViewChild } from '@angular/core';
+import { Component, HostListener, Inject, OnInit, ViewChild } from '@angular/core';
 import { SelectItem } from 'primeng/api';
 import { DynamicDialogConfig, DynamicDialogRef } from 'primeng/dynamicdialog';
 import { Table } from 'primeng/table';
+import { AlertaService } from 'src/app/components/alert-dialog/alerta.service';
 import { ProdutoCatalogoPropriedadeOpcao } from 'src/app/dto/produtos-catalogo/ProdutoCatalogoPropriedadeOpcao';
 import { ProdutoTabela } from 'src/app/dto/produtos-catalogo/ProdutoTabela';
 import { Constantes } from 'src/app/utilities/constantes';
 import { MoedaUtils } from 'src/app/utilities/formatarString/moeda-utils';
 import { StringUtils } from 'src/app/utilities/formatarString/string-utils';
 import { MensagemService } from 'src/app/utilities/mensagem/mensagem.service';
+import { SweetalertService } from 'src/app/utilities/sweetalert/sweetalert.service';
 import { TelaDesktopBaseComponent } from 'src/app/utilities/tela-desktop/tela-desktop-base.component';
 import { TelaDesktopService } from 'src/app/utilities/tela-desktop/tela-desktop.service';
 
@@ -16,17 +18,22 @@ import { TelaDesktopService } from 'src/app/utilities/tela-desktop/tela-desktop.
   templateUrl: './select-evap-dialog.component.html',
   styleUrls: ['./select-evap-dialog.component.scss']
 })
-export class SelectEvapDialogComponent implements OnInit {
+export class SelectEvapDialogComponent extends TelaDesktopBaseComponent implements OnInit {
 
   constructor(@Inject(DynamicDialogConfig) public option: DynamicDialogConfig,
-    public ref: DynamicDialogRef) {
+    public ref: DynamicDialogRef,
+    private readonly alertaService: AlertaService,
+    private readonly mensagemService: MensagemService,
+    private readonly sweetAlertService: SweetalertService,
+    telaDesktopService: TelaDesktopService,
+  ) {
+    super(telaDesktopService)
   }
 
   @ViewChild('dataTable') table: Table;
   stringUtils = StringUtils;
   evaporadorasPassadas: ProdutoTabela[];
-  evaporadoras: ProdutoTabela[];
-  evaporadoraSelecionada: ProdutoTabela;
+  evaporadorasFiltradas: ProdutoTabela[];
   lstOpcoes: ProdutoCatalogoPropriedadeOpcao[];
   lstBtus: SelectItem[] = [];
   lstLinhaProdutos: SelectItem[] = [];
@@ -38,21 +45,19 @@ export class SelectEvapDialogComponent implements OnInit {
   btu: string;
   kcal: string;
   constantes = new Constantes();
+  selecionados: Array<ProdutoTabela> = new Array<ProdutoTabela>();
 
   ngOnInit(): void {
     this.evaporadorasPassadas = this.option.data.evaps;
+    if (this.evaporadorasPassadas && this.evaporadorasPassadas.length > 0) {
+      this.evaporadorasPassadas.forEach(x => {
+        x.qtde = 0;
+      })
+    }
     this.lstOpcoes = this.option.data.opcoes;
-    this.evaporadoras = this.evaporadorasPassadas;
     this.buscarLinhaProdutos();
     this.buscarBtus();
     this.buscarKcals();
-  }
-
-  addProduto() {
-    if (this.evaporadoraSelecionada) {
-      this.ref.close(this.evaporadoraSelecionada);
-    }
-    return;
   }
 
   buscarKcals() {
@@ -81,20 +86,157 @@ export class SelectEvapDialogComponent implements OnInit {
     });
   }
 
-  filtrarEvaporadoras() {
-    this.linhaProduto;
-    this.btu;
+  pesquisar() {
 
-    let evaporadorasFiltradas: ProdutoTabela[] = this.evaporadorasPassadas;
+    if (!this.linhaProduto && !this.btu && !this.kcal) {
+      this.alertaService.mostrarMensagem("Preencha pelo menos um filtro para pesquisa!");
+      return;
+    }
+
+    if (this.selecionados && this.selecionados.length > 0) {
+      this.sweetAlertService.dialogo("", "Os itens selecionados não foram adicionados.<br> Pretende continuar?").subscribe((r) => {
+        if (!r) {
+          return;
+        }
+        else {
+          this.selecionados = new Array<ProdutoTabela>();
+          this.buscarProdutos();
+          return;
+        }
+      });
+    } else {
+      this.buscarProdutos();
+    }
+
+  }
+
+  buscarProdutos() {
+    // this.evaporadorasPassadas.forEach(x =>{
+    //   x.qtde = 0;
+    // });
+
+    let evaporadorasFiltradas: ProdutoTabela[] = JSON.parse(JSON.stringify(this.evaporadorasPassadas));
 
     if (this.linhaProduto) evaporadorasFiltradas = evaporadorasFiltradas.filter(x => x.linhaBusca.includes(`|${this.constantes.fProp}${this.linhaProduto}|`));
     if (this.btu) evaporadorasFiltradas = evaporadorasFiltradas.filter(x => x.linhaBusca.includes(`|${this.constantes.fProp}${this.btu}|`));
     if (this.kcal) evaporadorasFiltradas = evaporadorasFiltradas.filter(x => x.linhaBusca.includes(`|${this.constantes.fProp}${this.kcal}|`));
 
-    this.evaporadoras = evaporadorasFiltradas;
+    this.evaporadorasFiltradas = evaporadorasFiltradas;
   }
 
-  marcarLinha(e: Event) {
-    e.stopImmediatePropagation();
+  addQtde(produto: ProdutoTabela) {
+
+    if (!this.selecionados) {
+      this.selecionados = new Array<ProdutoTabela>();
+    }
+
+    let linha = document.getElementById(`linha_tabela_${produto.produto}`) as HTMLElement;
+    linha.classList.add("p-highlight");
+
+    produto.qtde++;
+
+    let selecionado = this.selecionados.filter(x => x.produto == produto.produto)[0];
+    if (!selecionado) {
+      this.selecionados.push(produto);
+    }
+  }
+
+  subtrairQtde(produto: ProdutoTabela, index: number) {
+
+    if (!this.selecionados) return;
+    if (produto.qtde == 0) return;
+
+    if ((produto.qtde - 1) == 0) {
+      //desmarcar
+      let linha = document.getElementById(`linha_tabela_${produto.produto}`) as HTMLElement;
+      linha.classList.remove("p-highlight");
+      this.selecionados = this.selecionados.filter(x => x.produto != produto.produto);
+    }
+
+    produto.qtde--;
+  }
+
+  formatarQtde(e: Event, produto: ProdutoTabela): void {
+    let valor = ((e.target) as HTMLInputElement).value;
+    let v: any = valor.replace(/,/g, '');
+    v = valor.replace(/[^0-9]/g, '');
+    if (!v) {
+      v = 0;
+    }
+
+    if (!this.selecionados) {
+      this.selecionados = new Array<ProdutoTabela>();
+    }
+
+    let linha = document.getElementById(`linha_tabela_${produto.produto}`) as HTMLElement;
+    if (v == 0) {
+      linha.classList.remove("p-highlight");
+      this.selecionados = this.selecionados.filter(x => x.produto != produto.produto);
+    }
+    else {
+
+      let selecionado = this.selecionados.filter(x => x.produto == produto.produto)[0];
+      if (!selecionado) {
+        this.selecionados.push(produto);
+      }
+      linha.classList.add("p-highlight");
+    }
+    v = v
+    produto.qtde = Number.parseInt(v);
+    ((e.target) as HTMLInputElement).value = v;
+  }
+
+  @HostListener('document:keydown.enter', ['$event']) onKeydownHandler(event: KeyboardEvent) {
+    if (event.which == 13) {
+      let el = (event.srcElement) as HTMLElement;
+      if (el.getAttribute("id") != "pesquisar" && el.getAttribute("id") != undefined) return;
+
+
+      this.pesquisar();
+      event.cancelBubble = true;
+      event.stopPropagation();
+      event.preventDefault();//esse cara que fez a diferença
+      event.stopImmediatePropagation();
+    }
+  }
+
+  addProduto() {
+    if (this.selecionados && this.selecionados.length > 0) {
+      this.option.contentStyle(this.selecionados);
+
+      this.limparListaTela();
+
+      return;
+    }
+  }
+
+  limparListaTela() {
+    this.selecionados.forEach(x => {
+      let linha = document.getElementById(`linha_tabela_${x.produto}`) as HTMLElement;
+      linha.classList.remove("p-highlight");
+      ((linha.children[1].children[1]) as HTMLInputElement).value = "0";
+    });
+
+    this.evaporadorasFiltradas.forEach(x => {
+      x.qtde = 0;
+    });
+
+    this.selecionados = new Array<ProdutoTabela>();
+  }
+
+  fechar() {
+    if (this.selecionados && this.selecionados.length > 0) {
+      this.sweetAlertService.dialogo("", "Os itens selecionados não foram adicionados.<br> Pretende continuar?").subscribe((r) => {
+        if (!r) {
+          return;
+        }
+        else {
+          this.selecionados = new Array<ProdutoTabela>();
+          this.ref.close();
+        }
+      });
+    } else {
+      this.ref.close();
+    }
   }
 }
