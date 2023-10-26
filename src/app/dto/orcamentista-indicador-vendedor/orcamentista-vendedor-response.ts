35874392b13facf01a95587912fb60f6417dbf22
Gabriel Prada Teodoro <Gabriel.Teodoro@itssolucoes.com.br>
2023-10-26 16:30:35 -0300

    Ajuste de paginação e manter filtros para tela de listagem de usuários

---
+++ src/app/dto/orcamentista-indicador-vendedor/orcamentista-vendedor-response.ts
@@ -0,0 +1,11 @@
+export class OrcamentistaVendedorResponse{
+    id: number = 0;
+    nome: string = "";
+    indicador: number;
+    ativo: boolean = false;
+    email: string = "";
+    ativoLabel: string = "NÂO";
+    parceiro: string;
+    stringBusca:string;
+    vendedorResponsavel:string;
+}
---
+++ src/app/dto/usuarios/usuarios-request.ts
@@ -0,0 +1,12 @@
+export class UsuariosRequest{
+    parceiro:string;
+    vendedor:string;
+    tipoUsuario:number;
+    loja:string;
+    ativo:number;
+    pesquisa:string;
+    pagina:number;
+    qtdeItensPagina:number;
+    ordenacaoAscendente:boolean;
+    nomeColuna:string;
+}
